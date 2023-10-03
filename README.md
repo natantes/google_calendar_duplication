7f9e4cb0d4f37fd4d7602f69a0522c4bf9b342e0
natanmoon <75587079+natanmoon@users.noreply.github.com>
2023-10-03 18:06:09 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,30 @@
+<p align="center"><img height="128" width="128" src="./assets/icon128.png" alt="google-calendar-quick-duplicate" />
+</p>
+<h1 align="center">google-calendar-quick-duplicate</h1>
+
+> A simple Chrome Extension to quickly duplicate events on Google Calendar.
+
+
+<p align="center"><a href="https://chrome.google.com/webstore/detail/google-calendar-quick-dup/belnijodgolpgmpahmdkjbjehbobnfpd?hl=en"><img src="https://storage.googleapis.com/chrome-gcs-uploader.appspot.com/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/UV4C4ybeBTsZt43U4xis.png"></a></p>
+
+## How it works
+This extension adds a "Duplicate event" icon on the event preview card, letting you quickly duplicate it with one single click.
+
+You can also quickly duplicate the event by holding Alt (option on MacOS) while clicking the event.
+
+<p align="center">
+  <img src="./assets/second.png">
+</p>
+
+## Bugs and feature requests
+For any issues, bugs and feature requests feel free to [open an issue](https://github.com/fabiosangregorio/google-calendar-quick-duplicate/issues) on Github.
+
+## Versioning
+Releases versions on Github correspond to the relative release version on the Chrome Web Store.
+
+We follow Semantic Versioning. The version X.Y.Z indicates:
+
+* X is the major version (backward-incompatible),
+* Y is the minor version (backward-compatible), and
+* Z is the patch version (backward-compatible bug fix).
+
---
+++ app.js
@@ -0,0 +1,364 @@
+"use strict";
+
+/** ========== CONSTANTS ========== */
+const GCQD_DUPLICATE_BUTTON_CLASS = "dup-btn";
+const GCQD_DUPLICATE_BUTTON_SELECTOR = `.${GCQD_DUPLICATE_BUTTON_CLASS}`;
+const CIRCLE_BUTTON_CLASS = "VbA1ue";
+const CALENDAR_EVENT_SELECTOR = ".NlL62b[data-eventid]";
+const EVENT_PANEL_SELECTOR = ".pPTZAe";
+const OPTIONS_BUTTON_SELECTOR = '.d29e1c';
+const SAVE_BUTTON_SELECTOR = '[jsname="x8hlje"]';
+const DUPLICATE_BUTTON_SELECTOR = '[jsname="lbYRR"]';
+const MINI_CALENDAR_DAY_SELECTOR = ".W0m3G";
+const MINI_CALENDAR_CURRENT_DAY_SELECTOR = ".folmac";
+const START_TIME_SELECTOR = "[aria-label='Start time']";
+const END_TIME_SELECTOR = "[aria-label='End time']";
+const DATE_SELECTOR = ".e0yuIb";
+const INTERVAL_DELAY = 100;
+/** ======================================== */
+
+/** ========== MY CUSTOM FUNCTIONS ========== */
+
+
+function formatTime(timeStr) {
+  let hour = Number(timeStr.slice(1, 3));
+  let minute = timeStr.slice(3, 5);
+  let suffix = hour >= 12 ? 'pm' : 'am';
+  if (hour > 12) hour -= 12;
+  return `${hour}${minute === '00' ? '' : ':' + minute}${suffix}`;
+}
+
+
+function addFifteenMinutes(time) {
+  if (time) {
+    const timePart = time.substring(1, 7); // get the part after 'T' and before 'Z'
+    const hourPart = parseInt(timePart.substring(0, 2));
+    const minutePart = parseInt(timePart.substring(2, 4));
+    let newMinute = minutePart + 15;
+
+    let newHour = hourPart;
+    if (newMinute >= 60) {
+      newMinute -= 60;
+      newHour += 1;
+    }
+
+    if (newHour >= 24) { // check if it is next day
+      newHour -= 24;
+    }
+
+    const newTimePart =
+      newHour.toString().padStart(2, '0') +
+      newMinute.toString().padStart(2, '0') +
+      '00';
+
+    const newTime = 'T' + newTimePart;
+
+    return newTime;
+  } else {
+    console.log('Invalid time passed to addFifteenMinutes. Make sure "data-ical" attribute exists and is correctly formatted.');
+    return time;
+  }
+}
+
+function convertTimeFormat(inputTime) {
+  // Remove "am" or "pm" and convert to 24-hour format
+  var hour = parseInt(inputTime.substring(0, inputTime.indexOf(':')));
+  var minutes = inputTime.substring(inputTime.indexOf(':') + 1, inputTime.length - 2);
+  var period = inputTime.substring(inputTime.length - 2);
+  if (period.toLowerCase() === "pm" && hour < 12) {
+      hour += 12;
+  } else if (period.toLowerCase() === "am" && hour === 12) {
+      hour -= 12;
+  }
+  // Convert to strings and pad with zeros
+  var hours = ("0" + hour).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  // Format and return the time
+  return "T" + hours + minutes + "00";
+}
+
+function getMonthFromString(mon){
+
+  var d = Date.parse(mon + "1, 2012");
+  if(!isNaN(d)){
+    return new Date(d).getMonth() + 1;
+  }
+  return -1;
+}
+
+/** ========== TEMPLATES ========== */
+/**
+ * Creates the duplicate event button.
+ *
+ * An event named "Lunch" should have a dark circle button because there is an
+ * image behind it.
+ *
+ * @param {boolean} hasCircleBtn - true if the event has a circle button
+ */
+function getDuplicateButton(eventId, hasCircleBtn) {
+  return `
+    <div class="${GCQD_DUPLICATE_BUTTON_CLASS}" data-id="${eventId}">
+      <div>
+        ${hasCircleBtn ? `<div class="${CIRCLE_BUTTON_CLASS}"></div>` : ""}
+        <div id="duplicate-event" class="uArJ5e Y5FYJe ${
+          hasCircleBtn ? `A1NRff` : `cjq2Db`
+        } d29e1c"
+          jslog="74327; 2:[&quot;43aadic948fhep9u1afopevksk&quot;,&quot;acd52hqnogl5bokq04klqkdqfg@group.calendar.google.com&quot;,false,null,0,0,null,null,0,false,[1,2,null,[&quot;&quot;,2]],false]; track:JIbuQc"
+          jscontroller="VXdfxd"
+          jsaction="mouseenter:tfO1Yc; mouseleave:JywGue;touchstart:p6p2H; focus:AHmuwe; blur:O22p3e;"
+          jsshadow="" jsname="VkLyEc" 
+          aria-label="Duplicate event" aria-disabled="false" tabindex="0"
+          data-tooltip="Duplicate event" data-tooltip-vertical-offset="-12" data-tooltip-horizontal-offset="0">
+          <div class="PDXc1b MbhUzd"></div>
+          <span  class="XuQwKc">
+            <span class="GmuOkf">
+              <svg height="20" viewBox="0 0 24 24" width="20" focusable="false" class=" NMm5M"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4H8c-1.1 0-1.99.9-1.99 2L6 21c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V11l-6-6zM8 21V7h6v5h5v9H8z"/></svg>
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+// Hide event menu when extension is active
+const cssStyle = `
+.gcqd-active .JPdR6b.Q3pIde.qjTEB {
+  display: none;
+}
+`;
+/** ======================================== */
+
+/** ========== MAIN FUNCTION ========== */
+let intervalInjectIcon;
+let intervalDuplicateEvent;
+let intervalGoToDay;
+let intervalSaveEvent;
+let currentDate = "";
+
+function app() {
+  appendStyleTag(cssStyle);
+
+  addEvent(document, "click", CALENDAR_EVENT_SELECTOR, (e) => {
+    injectDuplicateButton(e);
+    setUpShortcut(e);
+  });
+
+  addEvent(document, "click", GCQD_DUPLICATE_BUTTON_SELECTOR, () => {
+    duplicateEvent();
+  });
+}
+
+/**
+ * Injects the duplicate button in the event panel when the user clicks on the
+ * event.
+ */
+function injectDuplicateButton(event) {
+  const eventId = event.target.getAttribute("data-eventid");
+
+  intervalInjectIcon = setInterval(function () {
+    const eventPanelNode = document.querySelector(EVENT_PANEL_SELECTOR);
+    if (eventPanelNode == null) return;
+
+    clearInterval(intervalInjectIcon);
+    const duplicateButton = eventPanelNode.querySelector(
+      GCQD_DUPLICATE_BUTTON_SELECTOR
+    );
+    // Inject the button if it's not already there
+    if (duplicateButton == null) {
+      prependDuplicateButton(eventPanelNode, eventId);
+    }
+  }, INTERVAL_DELAY);
+}
+
+/** Prepends the duplicate button to the event panel buttons list. */
+function prependDuplicateButton(eventPanelNode, eventId) {
+  const hasCircleBtn = hasCircleButton(eventPanelNode);
+  const duplicateButton = getDuplicateButton(eventId, hasCircleBtn);
+  eventPanelNode.prepend(htmlToElement(duplicateButton));
+}
+
+/** Returns true if the event panel has circle buttons. */
+function hasCircleButton(eventPanelNode) {
+  return eventPanelNode.querySelector(`.${CIRCLE_BUTTON_CLASS}`) != null;
+}
+
+function duplicateEvent() {
+  document.body.classList.add("gcqd-active");
+  clearInterval(intervalDuplicateEvent);
+
+  intervalDuplicateEvent = setInterval(function () {
+    var optionsButton = document.querySelector(OPTIONS_BUTTON_SELECTOR);
+    var duplicateButton = document.querySelector(DUPLICATE_BUTTON_SELECTOR);
+
+    // Open the options menu if it's closed, then click the duplicate button.
+    if (isOptionsMenuClosed(optionsButton, duplicateButton)) {
+      simulateClick(optionsButton);
+    } else if (duplicateButton != null) {
+      currentDate = document
+        .querySelector(MINI_CALENDAR_CURRENT_DAY_SELECTOR)
+        .getAttribute("data-date");
+      simulateClick(duplicateButton);
+      saveEvent();
+    }
+  }, INTERVAL_DELAY);
+}
+
+/**
+ * Returns true if the options menu inside an event panel is closed, true
+ * otherwise.
+ */
+function isOptionsMenuClosed(optionsButton, duplicateButton) {
+  return optionsButton != null && duplicateButton == null;
+}
+
+/** Saves the duplicated event when the save modal has opened. */
+function saveEvent() {
+  clearInterval(intervalSaveEvent);
+  let newStartTime = null;
+  let newEndTime = null;
+
+  intervalSaveEvent = setInterval(function () {
+    let saveButton = document.querySelector(SAVE_BUTTON_SELECTOR);
+    let startTimeElement = document.querySelector(START_TIME_SELECTOR);
+    if (startTimeElement) { 
+      const startValue = startTimeElement.value;
+      startTimeElement.click();
+      newStartTime = addFifteenMinutes(convertTimeFormat(startValue));
+      let startTimeButton = document.querySelector(`div[data-ical="${newStartTime}"]`);
+
+      if (startTimeButton) {
+        startTimeButton.click();
+      } else {
+        console.log('No button for the desired start time.');
+      }
+
+      let endTimeElement = document.querySelector(END_TIME_SELECTOR);
+      endTimeElement.click();
+  
+      newEndTime = addFifteenMinutes(newStartTime);
+      let endTimeButton = document.querySelectorAll(`div[data-ical="${newEndTime}"]`)
+      endTimeButton = endTimeButton[1]
+  
+      if (endTimeButton) {
+        endTimeButton.click();
+      } else {
+        console.log('No button for the desired end time.');
+      }
+
+    } else {
+      console.log("no start time element")
+    }
+
+    if (saveButton == null) return;
+
+    clearInterval(intervalDuplicateEvent);
+    clearInterval(intervalSaveEvent);
+    saveButton.click();
+
+    goToCurrentDate();
+  }, INTERVAL_DELAY);
+}
+
+/**
+ * Returns to the date the user was to prior to duplicating the event.
+ *
+ * NOTE: this is needed because sometimes the calendar jumps back to today after
+ * duplicating the event.
+ * FIXME: this sometimes doesn't work.
+ */
+function goToCurrentDate() {
+  clearInterval(intervalGoToDay);
+
+  intervalGoToDay = setInterval(function () {
+    if (location.href.includes("duplicate")) return;
+    clearInterval(intervalGoToDay);
+
+    const todayDate = padDate(new Date());
+    if (currentDate !== todayDate) {
+      // FIXME: doing this when duplicating an event on today date results in a
+      // change of view
+      const miniDay = document.querySelector(MINI_CALENDAR_DAY_SELECTOR);
+      const miniWeek = miniDay.parentNode;
+      const clonedDay = miniDay.cloneNode();
+      clonedDay.setAttribute("data-date", currentDate);
+      miniWeek.append(clonedDay);
+      clonedDay.click();
+      clonedDay.remove();
+    }
+
+    document.body.classList.remove("gcqd-active");
+  }, INTERVAL_DELAY);
+}
+
+/** Triggers event duplication on shortcut click. */
+function setUpShortcut(event) {
+  if (event.altKey && !event.shiftKey && !event.ctrlKey) duplicateEvent();
+}
+
+/** ======================================== */
+
+/** ========== UTILITY FUNCTIONS ========== */
+/** Appends a style tag to the document's head containing the css in input. */
+function appendStyleTag(css) {
+  var styleTag = document.createElement("style");
+  document.head.appendChild(styleTag);
+  styleTag.type = "text/css";
+  styleTag.appendChild(document.createTextNode(css));
+}
+
+/** Pads a date with leading zeros. */
+function padDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  return year + String(month).padStart(2, "0") + String(day).padStart(2, "0");
+}
+
+function addEvent(parent, evt, selector, handler) {
+  parent.addEventListener(
+    evt,
+    function (event) {
+      if (event.target.matches(selector + ", " + selector + " *")) {
+        handler.apply(event.target.closest(selector), arguments);
+      }
+    },
+    false
+  );
+}
+
+/** Returns the element node corresponding to the html in input. */
+function htmlToElement(html) {
+  var template = document.createElement("template");
+  html = html.trim(); // Never return a text node with whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+/** Simulates a click by the user. */
+function simulateClick(element) {
+  element.click();
+  element.dispatchEvent(
+    new MouseEvent("mousedown", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    })
+  );
+  element.dispatchEvent(
+    new MouseEvent("mouseup", { bubbles: true, cancelable: true, view: window })
+  );
+}
+/** ======================================== */
+
+/** ========== READY EVENT ========== */
+if (
+  document.readyState === "complete" ||
+  (document.readyState !== "loading" && !document.documentElement.doScroll)
+) {
+  app();
+} else {
+  document.addEventListener("DOMContentLoaded", app);
+}
+/** ======================================== */
